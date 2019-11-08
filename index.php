66cc98c7f737896a346591b8f47c9c3396f14120
AdamAnsari <adam.a.ansari@gmail.com>
2019-11-08 18:59:59 +0530

    Completed all the folder and file setup

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Student Attendance Management</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+	
+	
+	<script src="js/index.js"></script>	
+</body>
+</html>
